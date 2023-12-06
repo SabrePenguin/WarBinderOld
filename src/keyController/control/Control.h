73ae3fb13a7420f5c4253e3c1e6fd8000f7b7f66
sabre_penguin <lknofczynski@gmail.com>
2023-12-05 21:21:08 -0500

    Added abstract class and removed unnecessary implementation

--- src/keyController/control/Control.h
+++ src/keyController/control/Control.h
@@ -1,4 +1,11 @@
 #ifndef CONTROL_H
 #define CONTROL_H
-
+#include <string>
+class Control
+{
+	public:
+		virtual int add_bind() {};
+		virtual bool set_local_key(std::string local_key) {};
+		virtual bool set_modifer(bool enable) {};
+};
 #endif
