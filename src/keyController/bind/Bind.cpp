dc907937b5d0a2efc653b00d8005aeb3d9347ae0
sabre_penguin <lknofczynski@gmail.com>
2023-12-07 14:49:33 -0500

    Added proper constructor and vector
    -Added Doxygen
    -Bind::add_control now uses vector<Control*>

--- src/keyController/bind/Bind.cpp
+++ src/keyController/bind/Bind.cpp
@@ -4,12 +4,15 @@
 #include "../control/Control.h"
 #include "../control/Key.h"
 
-Bind::Bind()
+Bind::Bind(std::string _internal_id, std::string _text_id) : internal_id(_internal_id), text_id(_text_id)
 {
 
 }
-
-void Bind::add_control(Control* _control)
+/**
+ * @brief Adds a vector of keys to the current Bind.
+ * @param _control_combo : vector of implemented Control classes
+*/
+void Bind::add_control(std::vector<Control*> _control_combo)
 {
 
 }
