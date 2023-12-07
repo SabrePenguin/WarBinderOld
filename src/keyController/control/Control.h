4d6650534d6dc046211164e97b8e435149234197
sabre_penguin <lknofczynski@gmail.com>
2023-12-06 19:56:38 -0500

    Added return types Used class forwarding Added endif variable name

--- src/keyController/control/Control.h
+++ src/keyController/control/Control.h
@@ -1,12 +1,14 @@
 #ifndef CONTROL_H
 #define CONTROL_H
 #include <string>
+#include <vector>
+class Bind;
 class Control
 {
 	public:
-		Control();
-		virtual int add_bind() {};
-		virtual bool set_local_key(std::string local_key) {};
-		virtual bool set_modifer(bool enable) {};
+		Control() {};
+		virtual void add_bind(Bind* _bind) {};
+		virtual bool set_local_key(std::string local_key) { return false; };
+		virtual bool set_modifer(bool enable) { return false; };
 };
-#endif
+#endif // CONTROL_H
