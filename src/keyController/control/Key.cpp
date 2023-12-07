9d2ce9ddceb36281c0541169419fa542f5227c33
sabre_penguin <lknofczynski@gmail.com>
2023-12-07 16:05:16 -0500

    Added second constructor
    -Added documentation for future

--- src/keyController/control/Key.cpp
+++ src/keyController/control/Key.cpp
@@ -4,20 +4,53 @@
 #include "Control.h"
 #include "../bind/Bind.h"
 
-Key::Key(int k_id, char local, bool mod) : Control(), key_id( k_id ), local_key( local ), modifier_key( mod ) {
+/**
+ * @brief Modifier key constructor
+ * @param _key_id: Key id
+ * @param _local_key: Local key ("backspace", "a", "1")
+ * @param _modifier: Modifier key, such as ctrl or alt
+*/
+Key::Key(int _key_id, std::string _local_key, bool _modifier) : 
+	Control(), key_id( _key_id ), local_key( _local_key ), modifier_key( _modifier ) {
 
 }
 
+/**
+ * @brief The non-modifier constructor
+ * @param _key_id: Key id
+ * @param _local_key: Local key ("backspace", "b", "2")
+*/
+Key::Key(int _key_id, std::string _local_key) :
+	Control(), key_id( _key_id ), local_key( _local_key ), modifier_key( false )
+{
+
+}
+
+/**
+ * @brief Adds a single Bind or Axis class to the internal vector
+ * @param _bind : A single Bind that gets added to the vector
+*/
 void Key::add_bind(Bind* _bind)
 {
 	binds.push_back(_bind);
 }
 
+/**
+ * @brief Sets the representative key to the actual key in the system (ie. A, enter, backspace)
+ * @param local_key: The local key in terms of a string to allow other keys
+ * @return Whether the addition was successful
+*/
 bool Key::set_local_key(std::string local_key)
 {
 	return true;
 }
 
+/**
+ * @brief Enables or disables whether the key is a modifier key.
+ * This enables using keys such as 'a' and 'z' like 'ctrl' and 'alt'
+ * @param enable: Enable modifier key
+ * @return The current state of the key
+*/
 bool Key::set_modifier(bool enable)
 {
 	return true;
