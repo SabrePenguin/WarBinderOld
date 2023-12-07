fe020702e1ea79570955704bd0989ff0b1cca731
sabre_penguin <lknofczynski@gmail.com>
2023-12-06 19:56:04 -0500

    Added testing for add_bind and Key pointers

--- src/keyController/KeyBindController.cpp
+++ src/keyController/KeyBindController.cpp
@@ -1,10 +1,18 @@
-#include "KeyBindController.h"
 #include <iostream>
+#include <vector>
+#include "KeyBindController.h"
+#include "Control.h"
 #include "Key.h"
+#include "Bind.h"
 
 using namespace std;
 
 void helloWorld() {
 	Key key(3, 'a', true);
+	Key* test_key = new Key(3, 'a', true);
+	Bind* test_bind = new Bind();
+	std::vector<Control*> keys;
+	test_key->add_bind(test_bind);
+	keys.push_back(test_key);
 	cout << "Hello World" << endl;
 }
