d7e5a4e2d51dfca06ebd18601be3a050918edf09
sabre_penguin <lknofczynski@gmail.com>
2023-12-07 16:06:08 -0500

    Fixed incorrect constructor

--- src/keyController/KeyBindController.cpp
+++ src/keyController/KeyBindController.cpp
@@ -10,7 +10,7 @@ using namespace std;
 void helloWorld() {
 	Key key(3, 'a', true);
 	Key* test_key = new Key(3, 'a', true);
-	Bind* test_bind = new Bind();
+	Bind* test_bind = new Bind(1, "AAA");
 	std::vector<Control*> keys;
 	test_key->add_bind(test_bind);
 	keys.push_back(test_key);
