8a36a15d20597cace0cc8c751781206c6974cf6a
sabre_penguin <lknofczynski@gmail.com>
2023-12-07 14:50:54 -0500

    Changed internal id and constructor
    -internal_id now uses string to allow for other characters ("backspace")
    -Constructor was expanded
    -add_control now uses a vector

--- src/keyController/bind/Bind.h
+++ src/keyController/bind/Bind.h
@@ -7,11 +7,14 @@ class Control;
 class Bind
 {
 	public:
-		Bind();
-		void add_control(Control* _control);
+		Bind(std::string _internal_id, std::string _text_id);
+		void add_control(std::vector<Control*> _control_combo);
 	private:
-		int id;
+		//The internal id of the bind (THROTTLE_UP)
+		std::string internal_id;
+		//The local name of the bind
 		std::string text_id;
+		//The collection of vectors that make up the controls
 		std::vector<std::vector<Control*>> control;
 };
 
