f617a08d7d8c2c25c9ad1e7a1005048574ca81fe
sabre_penguin <lknofczynski@gmail.com>
2023-12-08 15:34:38 -0500

    Fixed functions to use string
    -Added new functions
    -Added maps

--- src/keyController/KeyBindController.h
+++ src/keyController/KeyBindController.h
@@ -2,20 +2,27 @@
 #define KEYBINDCONTROL_H
 #include <string>
 #include <vector>
+#include <unordered_map>
 
-void helloWorld();
 class Control;
 class Bind;
 class KeyBindController 
 {
 	public:
-		KeyBindController();
+		KeyBindController(std::string _language);
 		~KeyBindController();
-		void add_key(int _key_id, std::string _local_key);
-		void add_modifier_key(int _key_id, std::string _local_key);
-		void add_new_control(int _key_id, std::string _local_key);
+		void add_key(std::string _key_id, std::string _local_key);
+		void add_modifier_key(std::string _key_id, std::string _local_key);
+		void add_new_control(std::string _key_id, std::string _local_key);
+		void add_new_bind(std::string _internal_id, std::string _local_id);
+		void set_language(std::string _language);
+		bool add_keys_to_bind(std::string _bind_name, std::vector<Control*> _added_keys);
 	private:
-		std::string name;
-		std::vector<Control*> system_keys;
+		//The language to be used. Will determine the csv column to use
+		std::string language;
+		//The map of system keys. Uses the id as the key, as each is guaranteed to be unique (O(1) time)
+		std::unordered_map<std::string, Control*> system_keys;
+		//The map of binds. Uses the local string id as the key to allow for searching later
+		std::unordered_map<std::string, Bind*> p_binds;
 };
 #endif // KEYBINDCONTROL_H
