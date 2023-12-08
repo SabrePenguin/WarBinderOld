73acd87c03f618a6abbcc18189572bdec6f54997
sabre_penguin <lknofczynski@gmail.com>
2023-12-07 23:14:34 -0500

    Added class declaration and forwarding

--- src/keyController/KeyBindController.h
+++ src/keyController/KeyBindController.h
@@ -1,1 +1,21 @@
+#ifndef KEYBINDCONTROL_H
+#define KEYBINDCONTROL_H
+#include <string>
+#include <vector>
+
 void helloWorld();
+class Control;
+class Bind;
+class KeyBindController 
+{
+	public:
+		KeyBindController();
+		~KeyBindController();
+		void add_key(int _key_id, std::string _local_key);
+		void add_modifier_key(int _key_id, std::string _local_key);
+		void add_new_control(int _key_id, std::string _local_key);
+	private:
+		std::string name;
+		std::vector<Control*> system_keys;
+};
+#endif // KEYBINDCONTROL_H
