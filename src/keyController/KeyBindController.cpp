9ec438ae812de27cb98a523538e61918a0f06bfd
sabre_penguin <lknofczynski@gmail.com>
2023-12-07 23:14:14 -0500

    Added constructor and deconstructor

--- src/keyController/KeyBindController.cpp
+++ src/keyController/KeyBindController.cpp
@@ -8,11 +8,19 @@
 using namespace std;
 
 void helloWorld() {
-	Key key(3, 'a', true);
-	Key* test_key = new Key(3, 'a', true);
-	Bind* test_bind = new Bind(1, "AAA");
+	Key key(3, "a", true);
+	Key* test_key = new Key(3, "a", true);
+	Bind* test_bind = new Bind("1", "AAA");
 	std::vector<Control*> keys;
 	test_key->add_bind(test_bind);
 	keys.push_back(test_key);
 	cout << "Hello World" << endl;
+}
+
+KeyBindController::KeyBindController() {
+
+}
+
+KeyBindController::~KeyBindController() {
+
 }
