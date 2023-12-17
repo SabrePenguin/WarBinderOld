53ce98235246825ab8420cbf1c273415315eb34a
sabre_penguin <lknofczynski@gmail.com>
2023-12-17 15:31:54 -0500

    Cleaned up data mistake

---
+++ src/WarBinder.cpp
@@ -0,0 +1,18 @@
+﻿// WarBinder.cpp : Defines the entry point for the application.
+//
+
+#include "WarBinder.h"
+#include "KeyBindController.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Starting Program" << endl;
+	KeyBindController key1("../../../../control_list.csv",
+		"../../../../wt_bind_list.csv",
+		"english");
+	//key1.add_new_bind("one_test", "One Test");
+	//key1.add_key("2", "hi");
+	return 0;
+}
