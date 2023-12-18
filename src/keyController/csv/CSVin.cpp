279e50485f3815a3eba9d34226cd71b776cb869a
sabre_penguin <lknofczynski@gmail.com>
2023-12-17 20:43:22 -0500

    Added parser column for gamemode

--- src/keyController/csv/CSVin.cpp
+++ src/keyController/csv/CSVin.cpp
@@ -13,9 +13,9 @@
  * @param _language: The language to import
  * @return A vector of tuples in the <id, local_name, modifier> format
 */
-std::vector<std::tuple<std::string, std::string, bool>> get_control(std::string _filename, std::string _language)
+std::vector<std::tuple<std::string, char, std::string, bool>> get_control( std::string _filename, std::string _language ) 
 {
-	std::vector<std::tuple<std::string, std::string, bool>> controls;
+	std::vector<std::tuple<std::string, char, std::string, bool>> controls;
 	{
 		std::ifstream in_file(_filename);
 		if( !in_file.is_open() )
@@ -25,6 +25,7 @@ std::vector<std::tuple<std::string, std::string, bool>> get_control(std::string 
 		std::string holder;
 		std::string key_id;
 		char c;
+		char mode ;
 		//Start base_index at -1 to allow for loop to work
 		int base_index = -1 ;
 		int local_index = 0 ;
@@ -40,7 +41,7 @@ std::vector<std::tuple<std::string, std::string, bool>> get_control(std::string 
 				//Runs once for the header to find the index
 				if( header )
 				{
-					if( in_string.compare( _language) == 0 )
+					if( in_string.compare( _language ) == 0 )
 					{
 						header = !header ;
 					}
@@ -49,6 +50,10 @@ std::vector<std::tuple<std::string, std::string, bool>> get_control(std::string 
 				else if( local_index == 0 )
 				{
 					key_id = in_string ;
+				}
+				else if( local_index == 1 )
+				{
+					mode = in_string[0] ;
 				}
 				//Proper column, setup for next column being true or false modifier. Can be elseif because of how the file works
 				else if( local_index == base_index ) 
@@ -72,7 +77,7 @@ std::vector<std::tuple<std::string, std::string, bool>> get_control(std::string 
 				{
 					correct_column = !correct_column ;
 					bool modifier = ( c == 'f' ) ;
-					controls.push_back( { key_id, holder, modifier } ) ;
+					controls.push_back( { key_id, mode, holder, modifier } ) ;
 					key_id = "";
 					holder = "";
 				}
