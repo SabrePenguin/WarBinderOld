344d666acfa3fd0a2e1231b35d9782493e66d763
sabre_penguin <lknofczynski@gmail.com>
2023-12-30 17:29:55 -0700

    Added abstract mode variable, and functions to handle them

--- src/keyController/control/Control.h
+++ src/keyController/control/Control.h
@@ -6,12 +6,18 @@ class Bind;
 class Control
 {
 	public:
-		Control() {};
+		Control(char _mode) : mode( _mode ) { } ;
 		virtual void add_bind(Bind* _bind) {};
 		virtual bool set_local_key(std::string local_key) { return false; };
 		virtual bool set_modifer(bool enable) { return false; };
+		char get_mode() { return mode ; } ;
+		void set_mode( char _mode ) { this->mode = _mode ; } ;
 		template <typename T>
 		T get_id() { return T(); };
 		virtual bool is_modifier() { return false; };
+	protected:
+		char mode;
+		//A list of the binds assigned to this key
+		std::vector<Bind*> binds;
 };
 #endif // CONTROL_H
