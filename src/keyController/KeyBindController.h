fbbbdbf97980b18aaa4e7bc4ae546c7de941873d
sabre_penguin <lknofczynski@gmail.com>
2024-01-02 11:47:20 -0700

    Changed to generic KeyBind

--- src/keyController/KeyBindController.h
+++ src/keyController/KeyBindController.h
@@ -5,7 +5,7 @@
 #include <unordered_map>
 
 class Control;
-class Bind;
+class KeyBind;
 class KeyBindController 
 {
 	public:
@@ -13,8 +13,9 @@ class KeyBindController
 		~KeyBindController();
 		void add_key(std::string _key_id, std::string _local_key, bool _modifier, char _gamemode);
 		void add_new_control(std::string _key_id, std::string _local_key);
-		void add_new_bind(std::string _internal_id, std::string _local_id, bool _axis, char _mode);
+		void add_new_bind(std::string _internal_id, std::string _local_id, char _mode, bool _is_axis);
 		void set_language(std::string _language);
+		void import(std::string _filename) ;
 		//bool add_keys_to_bind(std::string _bind_name, std::vector<Control*> _added_keys);
 	private:
 		//The language to be used. Will determine the csv column to use
@@ -22,6 +23,6 @@ class KeyBindController
 		//The map of system keys. Uses the id as the key, as each is guaranteed to be unique (O(1) time)
 		std::unordered_map<std::string, Control*> system_keys;
 		//The map of binds. Uses the local string id as the key to allow for searching later
-		std::unordered_map<std::string, Bind*> p_binds;
+		std::unordered_map<std::string, KeyBind*> p_binds;
 };
 #endif // KEYBINDCONTROL_H
