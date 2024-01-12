cdbeaaee23803c98e72ff0d61823f9c667022189
sabre_penguin <lknofczynski@gmail.com>
2024-01-12 10:12:09 -0500

    Added forward enum declaration

--- src/keyController/bind/KeyBind.h
+++ src/keyController/bind/KeyBind.h
@@ -4,13 +4,13 @@
 #include <string>
 
 class Control ;
-
+enum class controller ;
 class KeyBind
 {
 	public:
 		KeyBind(char _mode, bool _axis, bool _required) ;
-		virtual void add_control( std::vector<Control*> _control_combo, bool _up ) { return ; };
-		virtual void add_second_bind( std::string _text_id, bool _up ) { return ; } ;
+		virtual void add_control( std::vector<Control*> _control_combo, controller _up ) { return ; };
+		virtual void add_second_bind( std::string _text_id, controller _up ) { return ; } ;
 	protected:
 		//Unfortunate type check
 		bool axis ;
