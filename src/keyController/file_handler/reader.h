52da4d771c735ddbf81e45125ae0fd15f1d24bb7
sabre_penguin <lknofczynski@gmail.com>
2024-01-12 10:39:03 -0500

    Added enum and typedefs to header
    -struct Imported_Axis now uses Key_Type to determine if mouse or controller
    -import_controls now returns the t_return type for all data
    -t_return contains most data (not all) that will be returned (needs more work in reader.cpp)

--- src/keyController/file_handler/reader.h
+++ src/keyController/file_handler/reader.h
@@ -4,13 +4,25 @@
 #include <vector>
 #include <tuple>
 
+enum class Key_Type
+{
+	KEYBOARD,
+	MOUSE,
+	CONTROLLER
+};
+
 struct Imported_Axis {
+	Key_Type axis_type ;
 	std::string name, axis ;
 	bool inverted, relative, keep_value_for_disabled;
 	float dead_zone, multiplier, correction, relSens, relStep;
 	int non_linearity;
 };
 
-std::vector<std::tuple<std::string, std::vector<std::string>>> import_controls( std::string _filename ) ;
+typedef std::vector<std::tuple<Key_Type, std::string>> t_buttons ;
+typedef std::vector<std::tuple<std::string, t_buttons>> t_keys ;
+typedef std::tuple<t_keys, std::vector<Imported_Axis>> t_return ;
+
+t_return import_controls( std::string _filename ) ;
 
 #endif // !READER_H
