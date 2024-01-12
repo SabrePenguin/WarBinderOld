48928bd810b538a4ff604947ff980d69cfac4c44
sabre_penguin <lknofczynski@gmail.com>
2024-01-12 10:16:05 -0500

    Added new struct for easier data management

--- src/keyController/file_handler/reader.h
+++ src/keyController/file_handler/reader.h
@@ -4,6 +4,13 @@
 #include <vector>
 #include <tuple>
 
+struct Imported_Axis {
+	std::string name, axis ;
+	bool inverted, relative, keep_value_for_disabled;
+	float dead_zone, multiplier, correction, relSens, relStep;
+	int non_linearity;
+};
+
 std::vector<std::tuple<std::string, std::vector<std::string>>> import_controls( std::string _filename ) ;
 
 #endif // !READER_H
