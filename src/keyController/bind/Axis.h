9995ab2db76f95cc0b3457a8a6ef8ef5fe9fcfa5
sabre_penguin <lknofczynski@gmail.com>
2024-01-12 10:11:48 -0500

    Added enum and reset keyset

--- src/keyController/bind/Axis.h
+++ src/keyController/bind/Axis.h
@@ -2,12 +2,19 @@
 #define AXIS_H
 #include <string>
 #include "KeyBind.h"
+
+enum class controller
+{
+	INCREASE,
+	DECREASE,
+	RESET
+};
 class Axis: public KeyBind
 {
 	public:
-		Axis( std::string _internal_id, std::string _text_id, char _mode, bool _increase, bool _required);
-		void add_control( std::vector<Control*> _control_combo, bool _up );
-		void add_second_bind( std::string _text_id, bool _up ) ;
+		Axis( std::string _internal_id, std::string _text_id, char _mode, controller _increase, bool _required);
+		void add_control( std::vector<Control*> _control_combo, controller _direction );
+		void add_second_bind( std::string _text_id, controller _direction ) ;
 	private:
 		//The local name that represents the moving up of the axis
 		std::string increase_local ;
@@ -17,7 +24,9 @@ class Axis: public KeyBind
 		std::string decrease_local ;
 		//The collection of vectors that make up the increase control
 		std::vector<std::vector<Control*>> control_down;
-		//The internal id of the axis (throttle). Will auto adjust for both increase and decrease
+		std::string reset_local ;
+		std::vector<std::vector<Control*>> control_reset ;
+		//The internal id of the axis (ie. throttle). Will auto adjust for both increase, decrease, and reset
 		std::string internal_id ;
 		bool inverted;
 		bool relative;
