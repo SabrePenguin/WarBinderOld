c4a6f57175649fd62e8b3723522ca4e403525b70
sabre_penguin <lknofczynski@gmail.com>
2024-01-12 10:11:31 -0500

    Changed boolean to enum for reset key

--- src/keyController/bind/Axis.cpp
+++ src/keyController/bind/Axis.cpp
@@ -3,29 +3,35 @@
 #include "../control/Control.h"
 #include "Axis.h"
 
-Axis::Axis( std::string _internal_id, std::string _text_id, char _mode, bool _increase, bool _required ) :
+Axis::Axis( std::string _internal_id, std::string _text_id, char _mode, controller _increase, bool _required ) :
 	KeyBind( _mode, true, _required ), internal_id( _internal_id ), inverted( false ), relative( false ),
 	keep_value_for_disabled( false ), dead_zone( 0.0f ), non_linearity( 0 ), multiplier( 0.0f ),
 	correction( 0.0f ), relative_control_sensivity( 0.0f ), relative_control_step( 0.0f )
 {
-	if( _increase )
+	if( _increase == controller::INCREASE )
 		this->increase_local = _text_id ;
-	else
+	else if( _increase == controller::DECREASE )
 		this->decrease_local = _text_id ;
+	else
+		this->reset_local = _text_id ;
 }
 
-void Axis::add_control( std::vector<Control*> _control_combo, bool _up )
+void Axis::add_control( std::vector<Control*> _control_combo, controller _direction )
 {
-	if( _up )
+	if( _direction == controller::INCREASE )
 		this->control_up.push_back( _control_combo );
-	else
+	else if( _direction == controller::DECREASE )
 		this->control_down.push_back( _control_combo );
+	else
+		this->control_reset.push_back( _control_combo ) ;
 }
 
-void Axis::add_second_bind( std::string _text_id, bool _up )
+void Axis::add_second_bind( std::string _text_id, controller _direction )
 {
-	if( _up )
+	if( _direction == controller::INCREASE )
 		this->increase_local = _text_id ;
-	else
+	else if( _direction == controller::DECREASE )
 		this->decrease_local = _text_id ;
+	else
+		this->reset_local = _text_id ;
 }
