fadd340345ecf98b48f8592afcf09cda2772a111
sabre_penguin <lknofczynski@gmail.com>
2024-01-31 12:21:25 -0500

    Added add_bind implementation

--- src/keyController/control/Joystick.cpp
+++ src/keyController/control/Joystick.cpp
@@ -1,7 +1,13 @@
 #include "Joystick.h"
+#include "Joystick.h"
 
 Joystick::Joystick( std::string _joystick_id, std::string _local_name, bool _is_axis ) :
 	Control(), is_axis( _is_axis ), local_joy( _local_name ), joystick_id( _joystick_id )
 {
 
+}
+
+void Joystick::add_bind( KeyBind* _bind )
+{
+	this->binds.push_back( _bind ) ;
 }
--- src/keyController/control/Joystick.h
+++ src/keyController/control/Joystick.h
@@ -1,14 +1,16 @@
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
 #include "Control.h"
+class Keybind ;
 class Joystick : public Control
 {
 public:
 	Joystick( std::string _joystick_id, std::string _local_name, bool _is_axis ) ;
+	void add_bind( KeyBind* _bind ) ;
 private:
-	//The internal mouse button/axis id
+	//The internal joystick button/axis id
 	std::string joystick_id;
-	//The local mouse name
+	//The local joystick name
 	std::string local_joy;
 	//Indicates if the given id is an axis or not
 	bool is_axis ;
