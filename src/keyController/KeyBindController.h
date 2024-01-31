877e0a42effb1612385bb2974487e31be17e48f2
sabre_penguin <lknofczynski@gmail.com>
2024-01-31 12:21:39 -0500

    Added shared pointers, new joystick, and axis

--- src/keyController/KeyBindController.h
+++ src/keyController/KeyBindController.h
@@ -3,18 +3,22 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <memory>
 
 enum class controller ;
 enum class Key_Type ;
 class Control ;
 class KeyBind ;
+class Reader ;
+class Device ;
 class KeyBindController 
 {
 	public:
 		KeyBindController(std::string _controlfile, std::string _bindfile, std::string _language);
 		~KeyBindController();
 		void add_key(std::string _key_id, std::string _local_key, bool _modifier, char _gamemode);
-		void add_new_control(std::string _key_id, std::string _local_key);
+		void add_new_joystick(std::string _key_id, std::string _local_key);
+		void add_new_controller_axis( std::string _key_id, std::string _local_key ) ;
 		void add_new_bind(std::string _internal_id, std::string _local_id, char _mode, bool _is_axis, bool _required);
 		void set_language(std::string _language);
 		void import(std::string _filename) ;
@@ -25,8 +29,10 @@ class KeyBindController
 		//The language to be used. Will determine the csv column to use
 		std::string language;
 		//The map of system keys. Uses the id as the key, as each is guaranteed to be unique (O(1) time)
-		std::unordered_map<std::string, Control*> system_keys;
+		std::unordered_map<std::string, Control*> system_keys ;
 		//The map of binds. Uses the local string id as the key to allow for searching later
-		std::unordered_map<std::string, KeyBind*> p_binds;
+		std::unordered_map<std::string, KeyBind*> p_binds ;
+		std::unique_ptr<Reader> file_handler ;
+		std::unordered_map<std::string,std::shared_ptr<Device>> device_list ;
 };
 #endif // KEYBINDCONTROL_H
