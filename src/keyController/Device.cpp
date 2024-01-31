6d412cb27dfd6f60aa440a033ddc1cabdf9e4ac6
sabre_penguin <lknofczynski@gmail.com>
2024-01-31 12:21:31 -0500

    Setters and getters

--- src/keyController/Device.cpp
+++ src/keyController/Device.cpp
@@ -7,3 +7,47 @@ Device::Device( bool _connected, std::string _name, std::string _device_id,
 	type( _type )
 {
 }
+
+void Device::set_connected( bool _c )
+{
+	connected = _c ;
+}
+
+bool Device::get_connected()
+{
+	return connected ;
+}
+
+void Device::set_name( std::string _n )
+{
+	name = _n ;
+}
+
+void Device::set_id( std::string _i )
+{
+	device_id = _i ;
+}
+
+void Device::set_axes_offset( int _a )
+{
+	axes_offset = _a ;
+}
+
+void Device::set_axes_count( int _a )
+{
+	axes_count = _a ;
+}
+
+void Device::set_button_offset( int _b )
+{
+	button_offset = _b ;
+}
+
+void Device::set_button_count( int _b )
+{
+	button_count = _b ;
+}
+
+void Device::set_type( std::string _t )
+{
+}
