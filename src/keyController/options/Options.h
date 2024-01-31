17d63a96f5a07cc4b4b9d024bc4d8299971f3574
sabre_penguin <lknofczynski@gmail.com>
2024-01-31 12:22:06 -0500

    Documentation

--- src/keyController/options/Options.h
+++ src/keyController/options/Options.h
@@ -13,8 +13,11 @@ class Options
 		virtual std::tuple<T,std::string> get_value() ;
 		void set_value( T _value ) ;
 	private:
+		//The current value
 		T value ;
+		//The maximum value
 		T max ;
+		//Whether this exists in the lower or upper section of the .blk file
 		bool option ;
 };
 class Option_Integer : public Options<int>
@@ -23,6 +26,7 @@ class Option_Integer : public Options<int>
 		Option_Integer( int _cur_val, int _max, bool _option, std::list<std::string> _choices ) ;
 		std::tuple<int, std::string> get_value() ;
 	private:
+		//The list of choices, corresponding to the current value (hence why it's a list)
 		std::list<std::string> choices ;
 };
 #endif // !OPTIONS_H
