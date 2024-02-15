050ed94b59ced1ea23cf432f553ad87c45ae3595
sabre_penguin <lknofczynski@gmail.com>
2024-02-15 11:30:53 -0500

    Added basic functions

--- src/keyController/device_handler/DeviceImporter.h
+++ src/keyController/device_handler/DeviceImporter.h
@@ -1,6 +1,12 @@
 #ifndef DEVICE_IMPORTER_H
 #define DEVICE_IMPORTER_H
+#include <vector>
+class Device ;
 
-int get_keyboard_count() ;
+int startup() ;
+
+std::vector<Device> find_devices() ;
+
+void shutdown() ;
 
 #endif // !DEVICE_IMPORTER_H
