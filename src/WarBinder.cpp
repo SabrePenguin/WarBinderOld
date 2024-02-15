fc842a671f45447aa1bc72d6f96d992a5e0443da
sabre_penguin <lknofczynski@gmail.com>
2024-02-15 11:33:01 -0500

    Modified main to call program_loop.
    -Basic ptui added
    -Setup started for changing architecture to UML

--- src/WarBinder.cpp
+++ src/WarBinder.cpp
@@ -8,26 +8,73 @@
 #include <tuple>
 #include <vector>
 
-using namespace std;
+void initialize()
+{
+
+}
+
+void program_loop(KeyBindController* key_controller)
+{
+	bool active = true ;
+	char in ;
+	char data_type ;
+	while( active )
+	{
+		std::cout << "\nEnter one of the following choices:\nD(isplay)\nQ(uit)\n" << std::endl ;
+		std::cin >> in ;
+		switch( in )
+		{
+		//Display information
+		case 'D':
+		case 'd':
+			std::cout << "\nEnter one of the following choices:\nK(eys)\nB(inds)\n" << std::endl ;
+			std::cin >> data_type ;
+			//Key information
+			if( data_type == 'k' )
+			{
+				auto data = key_controller->get_key_details() ;
+				for( auto element = data.begin() ; element != data.end() ; element++ )
+				{
+					std::cout << "Local name: " << std::get<0>( *element ) << ", internal id: " << std::get<1>( *element ) << std::endl ;
+				}
+				break ;
+			}
+			//Bind information
+			else if( data_type == 'b' )
+			{
+				auto data2 = key_controller->get_bind_details() ;
+				for( auto element = data2.begin() ; element != data2.end() ; element++ )
+				{
+					std::cout << "Local name: " << std::get<0>( *element ) << ", internal id: " << std::get<1>( *element ) << std::endl ;
+				}
+			}
+			break ;
+
+		//Quit the loop
+		case 'Q':
+		case 'q':
+			active = false ;
+			break ;
+
+		//Repeat
+		default:
+			break ;
+		}
+	}
+}
 
 int main()
 {
-	cout << "Starting Program" << endl;
+	
+	std::cout << "Starting Program" << std::endl;
 	KeyBindController key1("../../../../control_list.csv",
 		"../../../../wt_bind_list.csv",
 		"../../../../options.csv",
 		"english");
 	//TODO: Check proper file type
 	key1.import( "../../../../controller_settings.blk" ) ;
-	auto data = key1.get_key_details() ;
-	for( auto element = data.begin() ; element != data.end() ; element++ )
-	{
-		std::cout << "Local name: " << std::get<0>( *element ) << ", internal id: " << std::get<1>( *element ) << std::endl ;
-	}
-	auto data2 = key1.get_bind_details() ;
-	for( auto element = data2.begin() ; element != data2.end() ; element++ )
-	{
-		std::cout << "Local name: " << std::get<0>( *element ) << ", internal id: " << std::get<1>( *element ) << std::endl ;
-	}
+	initialize() ;
+	program_loop( &key1 ) ;
+	
 	return 0;
 }
