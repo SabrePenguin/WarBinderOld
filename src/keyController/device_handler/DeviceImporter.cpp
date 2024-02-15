9f8c988b547594f3d23788ceff1d3c13cf7d33f1
sabre_penguin <lknofczynski@gmail.com>
2024-02-15 11:30:33 -0500

    Added basic SDL

--- src/keyController/device_handler/DeviceImporter.cpp
+++ src/keyController/device_handler/DeviceImporter.cpp
@@ -1,6 +1,37 @@
 #include <SDL.h>
+#include <iostream>
+#include <vector>
+#include "Device.h"
 
-int get_keyboard_count()
+int startup()
 {
-	return 1 ;
+	if( SDL_Init( SDL_INIT_GAMECONTROLLER ) < 0 )
+	{
+		std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl ;
+		return 1 ;
+	}
+	return 0 ;
+}
+
+std::vector<Device> find_devices()
+{
+	std::vector<Device> device_list ;
+	int joy_count = SDL_NumJoysticks() ;
+	if( joy_count > 0 )
+	{
+		for( int iter = 0 ; iter < joy_count ; iter++ )
+		{
+			if( SDL_IsGameController( iter ) )
+			{
+				SDL_GameController* controller = SDL_GameControllerOpen( iter ) ;
+				SDL_GameControllerClose( controller ) ;
+			}
+		}
+	}
+	return device_list ;
+}
+
+void shutdown()
+{
+	SDL_Quit() ;
 }
