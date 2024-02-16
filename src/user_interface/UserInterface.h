0ce2eebe4422e341a11ca33956d0dd606d619c7d
sabre_penguin <lknofczynski@gmail.com>
2024-02-16 17:41:59 -0500

    Added comments for memory and controller change receiver

--- src/user_interface/UserInterface.h
+++ src/user_interface/UserInterface.h
@@ -4,7 +4,12 @@
 class UserInterface
 {
 public:
-
+//Gets updated through an update()
+//Call Binder, which then calls relevant controller. May get assigned here instead.
+//DeviceHandler is a notifier, which notifies this that the controller is gone.
+//Model contains SDL loop
+//Gets information by calling relevant controller. Will be assigned here.
+	virtual void controller_change_notify() ;
 private:
 
 };
