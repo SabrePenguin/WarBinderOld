f56728b50d118a0a607ee20c29609c9ba23a81aa
sabre_penguin <lknofczynski@gmail.com>
2024-02-16 16:38:06 -0500

    Renamed, class added, added fields

---
+++ src/keyController/device_handler/DeviceHandler.h
@@ -0,0 +1,24 @@
+#ifndef DEVICE_HANDLER_H
+#define DEVICE_HANDLER_H
+#include <vector>
+#include <SDL.h>
+#include <memory>
+class Device ;
+class UserInterface ;
+
+class DeviceHandler
+{
+public:
+	DeviceHandler() ;
+	int startup() ;
+	std::vector<Device> find_devices() ;
+	void shutdown() ;
+	void add_ui_observer( UserInterface* _ui ) ;
+	void device_change( SDL_Event* con_event ) ;
+private:
+	/*The observer for the ui. Made a vector in case of wierd stuff.*/
+	std::vector<std::unique_ptr<UserInterface>> ui_observer ;
+	std::vector<std::unique_ptr<SDL_GameController>> game_controllers ;
+};
+
+#endif // !DEVICE_HANDLER_H
