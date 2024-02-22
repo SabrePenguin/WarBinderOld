6669507afa45eaf48c8b3898f22bab58cfc987d8
sabre_penguin <lknofczynski@gmail.com>
2024-02-22 10:38:22 -0500

    Added add and remove device

--- src/keyController/device_handler/DeviceHandler.h
+++ src/keyController/device_handler/DeviceHandler.h
@@ -15,6 +15,8 @@ public:
 	void shutdown() ;
 	void add_ui_observer( UserInterface* _ui ) ;
 	void device_change( SDL_Event* con_event ) ;
+	void remove_device( int index ) ;
+	void add_device( int index ) ;
 private:
 	/*The observer for the ui. Made a vector in case of wierd stuff.*/
 	std::vector<std::shared_ptr<UserInterface>> ui_observer ;
