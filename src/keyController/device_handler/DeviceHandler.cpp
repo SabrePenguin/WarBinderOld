1ea7c5419e3b4912028968a0db5e62ac6a36be01
sabre_penguin <lknofczynski@gmail.com>
2024-02-22 10:38:07 -0500

    Added events, event init, and handlers

--- src/keyController/device_handler/DeviceHandler.cpp
+++ src/keyController/device_handler/DeviceHandler.cpp
@@ -16,11 +16,13 @@ DeviceHandler::DeviceHandler()
 */
 int DeviceHandler::startup()
 {
-	if( SDL_Init( SDL_INIT_GAMECONTROLLER ) < 0 )
+	if( SDL_Init( SDL_INIT_EVENTS | SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER ) < 0 )
 	{
 		std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl ;
 		return 1 ;
 	}
+	SDL_JoystickEventState( SDL_ENABLE ) ;
+	SDL_GameControllerEventState( SDL_ENABLE ) ;
 	return 0 ;
 }
 
@@ -73,8 +75,40 @@ void DeviceHandler::add_ui_observer( UserInterface* _ui )
 */
 void DeviceHandler::device_change( SDL_Event* con_event )
 {
+	if( con_event->type == SDL_CONTROLLERDEVICEADDED || con_event->type == SDL_JOYDEVICEADDED )
+	{
+		this->add_device( con_event->cdevice.which ) ;
+	}
+	else if( con_event->type == SDL_CONTROLLERDEVICEREMOVED || con_event->type == SDL_JOYDEVICEREMOVED )
+	{
+		this->remove_device( con_event->cdevice.which ) ;
+	}
 	for( auto iter = this->ui_observer.begin() ; iter != this->ui_observer.end() ; iter++ )
 	{
 		iter->get()->controller_change_notify() ;
+	}
+}
+
+void DeviceHandler::add_device( int index )
+{
+	if( SDL_IsGameController( index ) )
+	{
+		SDL_GameController* controller = SDL_GameControllerOpen( index ) ;
+		if( controller )
+		{
+			this->game_controllers.push_back( controller ) ;
+		}
+	}
+}
+
+void DeviceHandler::remove_device( int index )
+{
+	for( auto iter = game_controllers.begin() ; iter != game_controllers.end() ; iter++ )
+	{
+		if( SDL_JoystickInstanceID( SDL_GameControllerGetJoystick( *iter ) ) == index )
+		{
+			SDL_GameControllerClose( *iter ) ;
+			iter = game_controllers.erase( iter ) ;
+		}
 	}
 }
