6a28623954c77036d97403057bee1dfb6306c28d
sabre_penguin <lknofczynski@gmail.com>
2024-02-24 23:59:34 -0500

    Added main loop

--- src/user_interface/UserInterface.h
+++ src/user_interface/UserInterface.h
@@ -9,7 +9,10 @@ public:
 //DeviceHandler is a notifier, which notifies this that the controller is gone.
 //Model contains SDL loop
 //Gets information by calling relevant controller. Will be assigned here.
-	virtual void controller_change_notify() ;
+
+	// Informs the UI that the controllers been added or removed
+	virtual void controller_change_notify() {} ;
+	virtual void main_loop() {} ;
 private:
 
 };
