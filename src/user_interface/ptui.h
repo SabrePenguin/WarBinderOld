5572559981f797d589e45fc66c3cdd1612d7dd64
sabre_penguin <lknofczynski@gmail.com>
2024-02-26 15:47:14 -0500

    Added lines, constructor, shared_ptr, and main_loop

--- src/user_interface/ptui.h
+++ src/user_interface/ptui.h
@@ -1,12 +1,22 @@
 #ifndef PTUI_H
 #define PTUI_H
 #include "UserInterface.h"
+#include <string>
+
+class KeyBindController ;
+
 class ptui : public UserInterface
 {
 public:
-	ptui() ;
-	void main_loop( KeyBindController* _controller ) ;
+	ptui( std::shared_ptr<KeyBindController> _controller ) ;
+	void main_loop( std::shared_ptr<KeyBindController> _controller ) ;
 	void controller_change_notify() ;
+
+private:
+	std::string line_one ;
+	std::string line_two ;
+	std::string line_three ;
+	std::shared_ptr<KeyBindController> controller ;
 };
 
 #endif // !PTUI_H
