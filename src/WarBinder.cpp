2acc7f6ba59b20e1330606edcfc088f8ad61fc68
sabre_penguin <lknofczynski@gmail.com>
2024-02-26 15:48:08 -0500

    Changed to shared_ptr, removed debugging

--- src/WarBinder.cpp
+++ src/WarBinder.cpp
@@ -14,6 +14,15 @@
 #include <memory>
 
 
+#define _CRTDBG_MAP_ALLOC
+#include<iostream>
+#include <crtdbg.h>
+#ifdef _DEBUG
+#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#define new DEBUG_NEW
+#endif
+
+
 int initialize()
 {
 	if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER ) < 0 )
@@ -24,7 +33,7 @@ int initialize()
 	return 0 ;
 }
 
-void sdl_loop( KeyBindController* key_controller )
+void sdl_loop( std::shared_ptr<KeyBindController> key_controller )
 {
 	bool active = true ;
 
@@ -37,15 +46,12 @@ void sdl_loop( KeyBindController* key_controller )
 			switch( event.type )
 			{
 			case SDL_CONTROLLERDEVICEADDED:
-				std::cout << "Added device" << std::endl ;
 				key_controller->notify_device( &event ) ;
 				break ;
 			case SDL_CONTROLLERDEVICEREMOVED:
-				std::cout << "Removed device" << std::endl ;
 				key_controller->notify_device( &event ) ;
 				break ;
 			case SDL_USEREVENT:
-				std::cout << "Quitting SDL\n" ;
 				active = false ;
 				break ;
 			}
@@ -59,23 +65,23 @@ int main()
 {
 	if( initialize() )
 		return 1 ;
+	{
+		std::shared_ptr<KeyBindController> key1 = std::make_shared<KeyBindController>( "../../../../control_list.csv",
+			"../../../../wt_bind_list.csv",
+			"../../../../options.csv",
+			"english" ) ;
 
-	KeyBindController key1("../../../../control_list.csv",
-		"../../../../wt_bind_list.csv",
-		"../../../../options.csv",
-		"english");
+		//TODO: Check proper file type
+		key1.get()->import( "../../../../controller_settings.blk" ) ;
 
-	//TODO: Check proper file type
-	key1.import( "../../../../controller_settings.blk" ) ;
+		std::shared_ptr<UserInterface> user_interface = std::make_shared<ptui>(key1);
+		key1.get()->add_ui_observer( user_interface ) ;
 
-	std::shared_ptr<UserInterface> user_interface = std::make_shared<ptui>();
-	key1.add_ui_observer( user_interface ) ;
-	std::cout << "Starting loop\n" ;
-
-	std::thread ui( &UserInterface::main_loop, user_interface, &key1 ) ;
-	sdl_loop( &key1 ) ;
-	ui.join() ;
-	_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_DEBUG );
-	_CrtDumpMemoryLeaks();
+		std::thread ui( &UserInterface::main_loop, user_interface, key1 ) ;
+		sdl_loop( key1 ) ;
+		ui.join() ;
+	}
+	//_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_DEBUG );
+	//_CrtDumpMemoryLeaks();
 	return 0;
 }
