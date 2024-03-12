10e91534fadb463e055e8d519ca59b15e73508ec
sabre_penguin <lknofczynski@gmail.com>
2024-03-11 20:50:00 -0600

    KeyBind now replaces Bind as a real class

--- src/keyController/bind/KeyBind.h
+++ src/keyController/bind/KeyBind.h
@@ -15,16 +15,18 @@ struct Imported_Axis ;
 class KeyBind
 {
 	public:
-		KeyBind(char _mode, bool _axis, bool _required) ;
+		KeyBind(char _mode, bool _axis, bool _required, std::string _local_name, std::string _internal_id) ;
 		virtual ~KeyBind() ;
-		virtual void add_control( std::vector<Control*> _control_combo, controller _up ) { return ; };
-		virtual void add_second_bind( std::string _text_id, controller _up ) { return ; } ;
-		virtual void add_axis( Control* _axis ) { return ; } ;
-		virtual void add_data( Imported_Axis* _imported ) { return ; };
-		virtual void reset() { return ; } ;
-		virtual std::vector<std::vector<Control*>>* get_main_control() { return nullptr ; } ;
-		virtual std::vector<std::vector<Control*>>* get_low_control() { return nullptr ; } ;
-		virtual std::vector<std::vector<Control*>>* get_upper_control() { return nullptr ; } ;
+		virtual void add_control( std::vector<Control*> _control_combo ) ;
+		virtual void add_second_bind( std::string _text_id, controller _up ) ;
+		virtual void add_axis( Control* _axis ) ;
+		virtual void add_data( Imported_Axis* _imported ) ;
+		virtual void reset() ;
+		/**
+		 * @brief Gets the list of controls from the current Bind
+		 * @return A pointer to the vector of vectors
+		 */
+		std::vector<std::vector<Control*>>* get_control() ;
 		/**
 		 * @brief Removes the given Control pointer from all key combinations in the dedicated control
 		 * @param _key: The pointer to remove
@@ -43,6 +45,9 @@ class KeyBind
 		char mode ;
 		//Whether the given control is required to play the game (ie. Fire guns)
 		bool required ;
+		std::vector<std::vector<Control*>> control;
+		std::string local_id ;
+		std::string internal_id ;
 };
 
 #endif // !KEYBIND_H
