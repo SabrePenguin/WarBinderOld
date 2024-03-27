b112fde838ad525462edb531fc2f57f4e8b248f4
sabre_penguin <lknofczynski@gmail.com>
2024-03-27 17:30:02 -0400

    Added frame

--- src/user_interface/wx_widgets/WXGui.cpp
+++ src/user_interface/wx_widgets/WXGui.cpp
@@ -3,6 +3,7 @@
 #ifndef WX_PRECOMP
 	#include <wx/wx.h>
 #endif
+#include <wx/splitter.h>
 #include <memory>
 
 //Include the crt to track any memory leaks
@@ -10,9 +11,9 @@
 #include <wx/msw/msvcrt.h>      // redefines the new() operator 
 #endif
 
-wxBEGIN_EVENT_TABLE( testFrame, wxFrame )
-EVT_MENU( wxID_EXIT, testFrame::OnExit )
-EVT_MENU( wxID_ABOUT, testFrame::OnAbout )
+wxBEGIN_EVENT_TABLE( mainFrame, wxFrame )
+EVT_MENU( wxID_EXIT, mainFrame::OnExit )
+EVT_MENU( wxID_ABOUT, mainFrame::OnAbout )
 wxEND_EVENT_TABLE()
 
 WXGui::WXGui()
@@ -23,36 +24,91 @@ WXGui::WXGui()
 
 bool WXGui::OnInit()
 {
-
-	testFrame* frame = new testFrame( "Test", wxPoint( 50, 50 ), wxSize( 450, 450 ) ) ;
+	mainFrame* frame = new mainFrame( "Test", wxDefaultPosition, wxDefaultSize ) ;
 	frame->Show( true ) ;
 	return true ;
 }
 
 wxIMPLEMENT_APP_NO_MAIN(WXGui) ;
 
-testFrame::testFrame( const wxString& title, const wxPoint& pos, const wxSize& size )
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+mainFrame::mainFrame( const wxString& title, const wxPoint& pos, const wxSize& size )
 	: wxFrame( NULL, wxID_ANY, title, pos, size )
 {
-    wxMenu* menuFile = new wxMenu ;
-    menuFile->Append( wxID_EXIT ) ;
-    wxMenu* menuHelp = new wxMenu ;
-    menuHelp->Append( wxID_ABOUT ) ;
-    wxMenuBar* menuBar = new wxMenuBar ;
-    menuBar->Append( menuFile, "&File" ) ;
-    menuBar->Append( menuHelp, "&Help" ) ;
-    SetMenuBar( menuBar ) ;
-    CreateStatusBar() ;
-    SetStatusText( "Welcome to wxWidgets!" ) ;
+    this->create_bar() ;
+
+    /*
+    wxPanel* panel_top = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxSize( 200, 100 ) ) ;
+    panel_top->SetBackgroundColour( wxColor( 100, 100, 100 ) ) ;
+    wxPanel* panel_bottom = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxSize( 200, 100 ) )  ;
+    panel_bottom->SetBackgroundColour( wxColor( 100, 200, 0 ) ) ;
+
+
+    wxSplitterWindow* split = new wxSplitterWindow( panel_bottom, wxID_ANY, wxDefaultPosition, wxDefaultSize ) ;
+    wxPanel* panel_bottom_l = new wxPanel( split, wxID_ANY, wxDefaultPosition, wxSize( 100, 100 ) ) ;
+    panel_bottom_l->SetBackgroundColour( wxColor( 100, 100, 200 ) ) ;
+    wxPanel* panel_bottom_r = new wxPanel( split, wxID_ANY, wxDefaultPosition, wxSize( 100, 100 ) ) ;
+    panel_bottom_r->SetBackgroundColour( wxColor( 0, 100, 200 ) ) ;
+
+
+    split->SplitVertically( panel_bottom_l, panel_bottom_r ) ;
+    split->SetMinimumPaneSize( 100 ) ;
+    split->SetSashGravity( 0.5 ) ;
+
+    wxBoxSizer* sizer = new wxBoxSizer( wxVERTICAL ) ;
+    sizer->Add( panel_top, 1, wxEXPAND ) ;
+    wxBoxSizer* size_bottom = new wxBoxSizer( wxHORIZONTAL ) ;
+    size_bottom->Add( panel_bottom, 1, wxEXPAND ) ;
+    //size_bottom->Add( panel_bottom_r, 0, wxEXPAND ) ;
+    //sizer->Add( panel_bottom, 1, wxEXPAND ) ;
+    sizer->Add( size_bottom, 1, wxEXPAND ) ;
+    this->SetSizerAndFit( sizer ) ;
+    */
 }
 
-void testFrame::OnExit( wxCommandEvent& event )
+void mainFrame::OnExit( wxCommandEvent& event )
 {
     Close( true ) ;
 }
 
-void testFrame::OnAbout( wxCommandEvent& event )
+void mainFrame::OnAbout( wxCommandEvent& event )
 {
-    wxMessageBox( "This is a wxWidgets' Hello world sample",
-        "About Hello World", wxOK | wxICON_INFORMATION );
+    wxMessageBox( "This is a program that uses wxWidgets for a small, native implementation",
+        "About WarBinder", wxOK | wxICON_INFORMATION );
+}
+
+void mainFrame::create_bar()
+{
+    //Create the File menu
+    wxMenu* menuFile = new wxMenu ;
+    menuFile->Append( wxID_EXIT ) ;
+    wxMenuItem* import = new wxMenuItem( menuFile, wxID_ANY, "Import" ) ;
+    menuFile->Append( import ) ;
+    wxMenuItem* save = new wxMenuItem( menuFile, wxID_ANY, "Save" ) ;
+    menuFile->Append( save ) ;
+
+    //Create the Edit menu
+    wxMenu* edit = new wxMenu() ;
+    wxMenuItem* item = new wxMenuItem( edit, wxID_ANY, "Item" ) ;
+    edit->Append( item ) ;
+
+    //Create the Preferences menu
+    wxMenu* prefs = new wxMenu() ;
+    wxMenuItem* lang = new wxMenuItem( prefs, wxID_ANY, "Language") ;
+    prefs->Append( lang ) ;
+
+    //Create the help menu
+    wxMenu* menuHelp = new wxMenu ;
+    menuHelp->Append( wxID_ABOUT ) ;
+    wxMenuItem* help = new wxMenuItem( prefs, wxID_ANY, "Help" ) ;
+    menuHelp->Append( help ) ;
+
+    //Append the wxMenu items to the Menubar
+    wxMenuBar* menuBar = new wxMenuBar ;
+    menuBar->Append( menuFile, "&File" ) ;
+    menuBar->Append( edit, "&Edit" ) ;
+    menuBar->Append( prefs, "&Preferences" ) ;
+    menuBar->Append( menuHelp, "&Help" ) ;
+    SetMenuBar( menuBar ) ;
 }
--- src/user_interface/wx_widgets/WXGui.h
+++ src/user_interface/wx_widgets/WXGui.h
@@ -5,24 +5,52 @@
 	#include <wx/wx.h>
 #endif
 #include <memory>
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+class forward_declare;
+
+#include <wx/string.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/icon.h>
+#include <wx/menu.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/button.h>
+#include <wx/sizer.h>
+#include <wx/scrolwin.h>
+#include <wx/statbmp.h>
+#include <wx/panel.h>
+#include <wx/notebook.h>
+#include <wx/frame.h>
 
 class WXGui : public wxApp
 {
 public:
+	/**
+	 * @brief WarBinder implementation of wxApp
+	 */
 	WXGui() ;
+	/**
+	 * @brief The main logic startup
+	 * @return Success or failure
+	 */
 	virtual bool OnInit() ;
 
 private:
 	
 };
 
-class testFrame : public wxFrame
+class mainFrame : public wxFrame
 {
 public:
-	testFrame( const wxString& title, const wxPoint& pos, const wxSize& size );
+	mainFrame( const wxString& title, const wxPoint& pos, const wxSize& size );
 private:
 	void OnExit( wxCommandEvent& event );
 	void OnAbout( wxCommandEvent& event );
+	void create_bar() ;
 	wxDECLARE_EVENT_TABLE();
 };
 
