4b1ca5300a19adf3981897727b0f850fc2e73325
SabrePenguin <lknofczynski@gmail.com>
2024-04-13 13:29:15 -0400

    Removed debug block

--- src/keyController/KeyBindController.cpp
+++ src/keyController/KeyBindController.cpp
@@ -19,14 +19,6 @@
 
 #endif
 
-#define _CRTDBG_MAP_ALLOC
-#include<iostream>
-#include <crtdbg.h>
-#ifdef _DEBUG
-#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
-#define new DEBUG_NEW
-#endif
-
 // The purpose of this file is to be the surface level file that users
 //
 
